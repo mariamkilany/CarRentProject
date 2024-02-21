9dd97be539199fe60a68417fa71e3dd386140934
mariamkilany <mahmoud32043@gmail.com>
2024-02-21 17:34:29 +0200

    Custom Theme + Car Details

--- src/index.js
+++ src/index.js
@@ -6,14 +6,20 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import { RouterProvider } from "react-router";
 import { router } from "./app/router";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./ThemeStyles/themeStyles";
+import { Button } from "@mui/material";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-	<React.StrictMode>
-		<RouterProvider router={router}>
-			<Provider store={store}>
-				<App />
-			</Provider>
-		</RouterProvider>
-	</React.StrictMode>
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </RouterProvider>
+    </ThemeProvider>
+  </React.StrictMode>
 );
