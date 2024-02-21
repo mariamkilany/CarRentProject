14b5b81c65ec2e001632efe58ccdecad3b920a4c
yasmin <20191815@fue.edu.eg>
2024-02-21 18:18:54 +0200

    second commit

--- src/Pages/MainLayout.jsx
+++ src/Pages/MainLayout.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/navbar/Navbar";
+import Footer from "../Components/footer/Footer";
+import Asidebar from "../Components/asidebar/asidebar";
 
 const MainLayout = () => {
-	return (
-		<>
-			<Navbar/>
-			<Outlet />
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+
+      <Outlet />
+      <Footer />
+    </>
+  );
 };
 
 export default MainLayout;
