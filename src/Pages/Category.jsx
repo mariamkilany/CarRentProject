36ee69cd841891f5b26b5fc081e406ccbc56fc0c
mariamkilany <mahmoud32043@gmail.com>
2024-02-23 01:15:54 +0200

    Create Upload Button

--- src/Pages/Category.jsx
+++ src/Pages/Category.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Asidebar from "../Components/asidebar/asidebar";
 import { Container } from "@mui/material";
 import CarCard from "../Components/carCard/CarCard";
+import CloudinaryButton from "../Components/cloudinaryButton/CloudinaryButton";
 
 const Category = () => {
   return (
     <Container>
       <CarCard />
+      <CloudinaryButton />
     </Container>
   );
 };
