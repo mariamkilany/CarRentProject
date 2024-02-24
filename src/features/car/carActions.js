885cef857c2997f6d7c199438fcd9a9877e6d9d0
Gameel Allam <fadyallam8@gmail.com>
2024-02-24 14:23:38 +0200

    -Solve repeted code

--- src/features/car/carActions.js
+++ src/features/car/carActions.js
@@ -17,42 +17,20 @@ export const fetchCar = createAsyncThunk("car/fetchCar", async (_, thunkAPI) => 
 	}
 });
 
-export const getAllCarAction = createAsyncThunk(
-  "car/getAll",
-  async (_, thunkAPI) => {
-    const { rejectWithValue } = thunkAPI;
-    try {
-      const response = await axios.get(
-        "https://65d24788987977636bfc333b.mockapi.io/api/cars"
-      );
-      return response.data;
-    } catch (error) {
-      const message = error.response?.data || error.message;
-      return rejectWithValue(message);
-    }
-  }
-);
-
-export const deleteCarAction = createAsyncThunk(
-  "car/delete",
-  async (id, thunkAPI) => {
-    const { rejectWithValue } = thunkAPI;
-    try {
-      const response = await axios.delete(
-        `https://65d24788987977636bfc333b.mockapi.io/api/cars/${id}`
-      );
-      return id;
-    } catch (error) {
-      const message = error.response?.data || error.message;
-      return rejectWithValue(message);
-    }
-  }
-);
-
-export const deleteCarAction = createAsyncThunk("car/delete", async (id,thunkAPI) => {
+export const getAllCarAction = createAsyncThunk("car/getAll", async (_, thunkAPI) => {
 	const { rejectWithValue } = thunkAPI;
 	try {
-	
+		const response = await axios.get("https://65d24788987977636bfc333b.mockapi.io/api/cars");
+		return response.data;
+	} catch (error) {
+		const message = error.response?.data || error.message;
+		return rejectWithValue(message);
+	}
+});
+
+export const deleteCarAction = createAsyncThunk("car/delete", async (id, thunkAPI) => {
+	const { rejectWithValue } = thunkAPI;
+	try {
 		const response = await axios.delete(`https://65d24788987977636bfc333b.mockapi.io/api/cars/${id}`);
 		return id;
 	} catch (error) {
@@ -61,41 +39,35 @@ export const deleteCarAction = createAsyncThunk("car/delete", async (id,thunkAPI
 	}
 });
 
-export const addCarAction = createAsyncThunk("car/add", async (car,thunkAPI) => {
+export const addCarAction = createAsyncThunk("car/add", async (car, thunkAPI) => {
 	const { rejectWithValue } = thunkAPI;
 	try {
 		// console.log(car)
-	 await axios.post(`https://65d24788987977636bfc333b.mockapi.io/api/cars`,car);
-	return car;
-
+		await axios.post(`https://65d24788987977636bfc333b.mockapi.io/api/cars`, car);
+		return car;
 	} catch (error) {
 		const message = error.response?.data || error.message;
 		return rejectWithValue(message);
 	}
 });
-export const updateCarAction = createAsyncThunk("car/update", async ({id,updatedInfo},thunkAPI) => {
+export const updateCarAction = createAsyncThunk("car/update", async ({ id, updatedInfo }, thunkAPI) => {
 	const { rejectWithValue } = thunkAPI;
 	try {
 		// console.log(car)
-		const response = await axios.put(`https://65d24788987977636bfc333b.mockapi.io/api/cars/${id}`,updatedInfo);
+		const response = await axios.put(`https://65d24788987977636bfc333b.mockapi.io/api/cars/${id}`, updatedInfo);
 		return response.data;
 	} catch (error) {
 		const message = error.response?.data || error.message;
 		return rejectWithValue(message);
 	}
 });
-export const getCarById = createAsyncThunk(
-  "car/getCarById",
-  async (id, thunkAPI) => {
-    const { rejectWithValue } = thunkAPI;
-    try {
-      const response = await axios.get(
-        `https://65d24788987977636bfc333b.mockapi.io/api/cars/${id}`
-      );
-      return response.data;
-    } catch (error) {
-      const message = error.response?.data || error.message;
-      return rejectWithValue(message);
-    }
-  }
-);
+export const getCarById = createAsyncThunk("car/getCarById", async (id, thunkAPI) => {
+	const { rejectWithValue } = thunkAPI;
+	try {
+		const response = await axios.get(`https://65d24788987977636bfc333b.mockapi.io/api/cars/${id}`);
+		return response.data;
+	} catch (error) {
+		const message = error.response?.data || error.message;
+		return rejectWithValue(message);
+	}
+});
--- src/features/car/carSlice.js
+++ src/features/car/carSlice.js
@@ -1,13 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { fetchCar,deleteCarAction,getAllCarAction,addCarAction,updateCarAction } from "./carActions";
-import { deleteCarAction, getAllCarAction, getCarById } from "./carActions";
+import { fetchCar, deleteCarAction, getAllCarAction, addCarAction, updateCarAction, getCarById } from "./carActions";
 
 const initialState = {
-  car: [],
-  loading: false,
-  error: null,
-  //   errorMessage: null,
-  selectedCar: null,
+	car: [],
+	loading: false,
+	error: null,
+	//   errorMessage: null,
+	selectedCar: null,
 };
 
 const carSlice = createSlice({
@@ -29,45 +28,45 @@ const carSlice = createSlice({
 			state.errorMessage = action.payload;
 		});
 		builder.addCase(addCarAction.fulfilled, (state, action) => {
-			 state.car.push(action.payload);
+			state.car.push(action.payload);
 		});
 		builder.addCase(updateCarAction.fulfilled, (state, action) => {
-			console.log(action.payload.id)
-			const filteredCar = state.car.findIndex(car=>car.id==action.payload.id)
-			console.log(filteredCar)
-			state.car[filteredCar] =action.payload;
+			console.log(action.payload.id);
+			const filteredCar = state.car.findIndex(car => car.id == action.payload.id);
+			console.log(filteredCar);
+			state.car[filteredCar] = action.payload;
 		});
-    builder.addCase(getAllCarAction.fulfilled, (state, action) => {
-      state.car = action.payload;
-    });
-    builder.addCase(getAllCarAction.pending, (state, action) => {
-      state.loading = true;
-    });
-    builder.addCase(getAllCarAction.rejected, (state, action) => {
-      state.loading = false;
-      state.error = true;
-    });
-    builder.addCase(deleteCarAction.fulfilled, (state, action) => {
-      state.car = state.car.filter((car) => car.id !== action.payload);
-    });
-    builder.addCase(deleteCarAction.pending, (state, action) => {
-      state.loading = true;
-    });
-    builder.addCase(deleteCarAction.rejected, (state, action) => {
-      state.loading = false;
-      state.error = true;
-    });
-    builder.addCase(getCarById.fulfilled, (state, action) => {
-      state.loading = false;
-      state.selectedCar = state.payload;
-    });
-    builder.addCase(getCarById.pending, (state, action) => {
-      state.loading = true;
-    });
-    builder.addCase(getCarById.rejected, (state, action) => {
-      state.loading = false;
-    });
-  },
+		builder.addCase(getAllCarAction.fulfilled, (state, action) => {
+			state.car = action.payload;
+		});
+		builder.addCase(getAllCarAction.pending, (state, action) => {
+			state.loading = true;
+		});
+		builder.addCase(getAllCarAction.rejected, (state, action) => {
+			state.loading = false;
+			state.error = true;
+		});
+		builder.addCase(deleteCarAction.fulfilled, (state, action) => {
+			state.car = state.car.filter(car => car.id !== action.payload);
+		});
+		builder.addCase(deleteCarAction.pending, (state, action) => {
+			state.loading = true;
+		});
+		builder.addCase(deleteCarAction.rejected, (state, action) => {
+			state.loading = false;
+			state.error = true;
+		});
+		builder.addCase(getCarById.fulfilled, (state, action) => {
+			state.loading = false;
+			state.selectedCar = state.payload;
+		});
+		builder.addCase(getCarById.pending, (state, action) => {
+			state.loading = true;
+		});
+		builder.addCase(getCarById.rejected, (state, action) => {
+			state.loading = false;
+		});
+	},
 });
 
 export default carSlice.reducer;
