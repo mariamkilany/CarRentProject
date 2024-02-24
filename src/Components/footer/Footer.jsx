8fb405dd75ab554d11e4ccd865be4cf0c06b26f7
mariamkilany <mahmoud32043@gmail.com>
2024-02-24 12:35:27 +0200

    Fix Footer

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,8 +1,10 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Unstable_Grid2";
-import { Divider, ListItemText, Stack, Typography } from "@mui/material";
-import { ListItemButton } from "@mui/material";
+import { IconButton, ListItemText, Stack, Typography } from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GoogleIcon from "@mui/icons-material/Google";
 
 const Footer = () => {
   const mystyle = { textAlign: "left", fontSize: "15px" };
@@ -14,9 +16,9 @@ const Footer = () => {
       }}
     >
       <Box>
-        <Grid container spacing={1}>
-          <Grid xs={12} md={5} lg={4}>
-            <ListItemText sx={{ marginLeft: "80px", paddingRight: "100px" }}>
+        <Grid container p={3}>
+          <Grid xs={12} md={6}>
+            <ListItemText>
               <Typography variant="h3" sx={{ color: "#3563E9" }}>
                 <b> MORENT</b>
               </Typography>
@@ -26,70 +28,8 @@ const Footer = () => {
               </Typography>
             </ListItemText>
           </Grid>
-
           <Grid
-            container
-            xs={12}
-            md={7}
-            lg={8}
-            sx={{ justifyContent: "center", textAlign: "center" }}
-          >
-            <Grid xs={12} lg={3}>
-              <ListItemText>
-                <Stack
-                  id="category-a"
-                  sx={{ fontSize: "20px", textAlign: "left", mb: 2 }}
-                >
-                  <b>About</b>
-                </Stack>
-                <Stack component="ul">
-                  <ListItemButton sx={mystyle}>How it works</ListItemButton>
-                  <ListItemButton sx={mystyle}>Featured</ListItemButton>
-                  <ListItemButton sx={mystyle}>Partnership</ListItemButton>
-                  <ListItemButton sx={mystyle}>
-                    Bussiness Relation
-                  </ListItemButton>
-                </Stack>
-              </ListItemText>
-            </Grid>
-            <Grid xs={12} lg={3}>
-              <ListItemText>
-                <Stack
-                  id="category-b"
-                  sx={{ fontSize: "20px", textAlign: "left", mb: 2 }}
-                >
-                  <b>Community</b>
-                </Stack>
-                <Stack component="ul">
-                  <ListItemButton sx={mystyle}>Event</ListItemButton>
-                  <ListItemButton sx={mystyle}>blog</ListItemButton>
-                  <ListItemButton sx={mystyle}>prodast</ListItemButton>
-                  <ListItemButton sx={mystyle}>Invite a friend</ListItemButton>
-                </Stack>
-              </ListItemText>
-            </Grid>
-            <Grid xs={12} lg={3} sx={{ alignItems: "left" }}>
-              <ListItemText>
-                <Stack
-                  id="category-c"
-                  sx={{ fontSize: "20px", textAlign: "left", mb: 2 }}
-                >
-                  <b>Socials</b>
-                </Stack>
-                <Stack component="ul" sx={{ alignItems: "left" }}>
-                  <ListItemButton sx={mystyle}>Discord</ListItemButton>
-                  <ListItemButton sx={mystyle}>Instagram</ListItemButton>
-                  <ListItemButton sx={mystyle}>Twitter</ListItemButton>
-                  <ListItemButton sx={mystyle}>Facebook</ListItemButton>
-                </Stack>
-              </ListItemText>
-            </Grid>
-          </Grid>
-          <Stack
-            divider={<Divider orientation="horizontal" color="error"></Divider>}
-          ></Stack>
-
-          <Grid
+            md={6}
             xs={12}
             container
             justifyContent="space-between"
@@ -97,11 +37,22 @@ const Footer = () => {
             flexDirection={{ xs: "column", sm: "row" }}
             sx={{ fontSize: "12px" }}
           >
-            <Grid sx={{ order: { xs: 2, sm: 1 } }}>
-              <Typography sx={{ marginLeft: "80px", fontSize: "15px" }}>
-                ©2022 MORENT. All rights reserved
-              </Typography>
-            </Grid>
+            <Stack
+              direction={"row"}
+              justifyContent={"right"}
+              gap={1}
+              width={"100%"}
+            >
+              <IconButton>
+                <FacebookIcon sx={{ fontSize: "35px" }} color="primary" />
+              </IconButton>
+              <IconButton>
+                <LinkedInIcon sx={{ fontSize: "35px" }} color="primary" />
+              </IconButton>
+              <IconButton>
+                <GoogleIcon sx={{ fontSize: "35px" }} color="primary" />
+              </IconButton>
+            </Stack>
           </Grid>
         </Grid>
       </Box>
